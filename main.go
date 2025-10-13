28320542d6707bcf4481bc0ad3355d934acc3dc2
nischay-chauhan <masternischay@outlook.com>
2025-10-13 11:01:13 +0530

    heatlh check

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-    "fmt"
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/redis/go-redis/v9"
 )
 
-var ctx = context.Background()	
+var ctx = context.Background()
 
 func main() {
 	rgb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     "localhost:6379",
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 
-	_,err := rgb.Ping(ctx).Result()
+	_, err := rgb.Ping(ctx).Result()
 	if err != nil {
 		fmt.Println("Failed to connect to Redis", err)
 		return
 	}
-
 	fmt.Println("Connected to Redis")
-	fmt.Println("Game leaderboard API")
+
+	http.HandleFunc("/health", healthCheck)
+	fmt.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
