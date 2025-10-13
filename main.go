fef6affbe9eb89d515c46a77f7ee5177dac32a49
nischay-chauhan <masternischay@outlook.com>
2025-10-13 10:59:25 +0530

    Redis connection

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
     "fmt"
+	"context"
+	"github.com/redis/go-redis/v9"
 )
 
+var ctx = context.Background()	
+
 func main() {
-    fmt.Println("Game leaderboard API")
+	rgb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	_,err := rgb.Ping(ctx).Result()
+	if err != nil {
+		fmt.Println("Failed to connect to Redis", err)
+		return
+	}
+
+	fmt.Println("Connected to Redis")
+	fmt.Println("Game leaderboard API")
 }
